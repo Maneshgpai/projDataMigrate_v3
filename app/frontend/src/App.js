08bdc9d2d518d4cfabed422fc331c810d68f1b24
Manesh Pai <maneshgpai@gmail.com>
2023-04-24 00:23:45 +0530

    CLean default React, remove default files

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import Default from "./components/error/Default";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Signup/>} />
+            <Route path="/signup" element={<Signup/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="*" element={<Default/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ app/frontend/src/components/error/Default.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Signup extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Error!!! Click browser back button</h1>
+      </Container>
+    );
+  }
+}
+
+export default Signup;
